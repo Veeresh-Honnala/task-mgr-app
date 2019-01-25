9cd98d377be68fd733dbe91a5f1b69c7a2cd4401
Veeresh Honnala <kumar.hv3@gmail.com>
2019-01-25 20:47:10 +0530

    added ui changes

---
+++ task-mgr-ui/src/app/shared/model/project.model.ts
@@ -0,0 +1,10 @@
+export class ProjectModel{
+    ProjectModel(
+        projectId?:number,
+        project?:string,
+        priority?:number,
+        manager?:string,
+        startDate?:Date,
+        endDate?:Date
+    ){}
+}
